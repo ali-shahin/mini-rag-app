f31f44ba96aa5421b619f87bbeb084e947e4af3c
ali <aly.shaheen14@gmail.com>
2025-05-10 15:29:09 +0300

    fix db connection

--- app/db.py
+++ app/db.py
@@ -2,14 +2,23 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import get_settings
 
 
-db = None
+db_client = None
 
 
 async def connect_to_mongo():
-    global client, db
+    global client, db_client
     client = AsyncIOMotorClient(get_settings().MONGO_DB_URL)
-    db = client[get_settings().MONGO_DB_NAME]
+    if client is None:
+        raise Exception("MongoDB not connected")
+
     print("✅ MongoDB connected")
+
+    db_client = client[get_settings().MONGO_DB_NAME]
+    if db_client is None:
+        raise Exception("Database not found")
+
+    print("✅ Database connected")
+    return db_client
 
 
 async def close_mongo_connection():
--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 from routes import base, data
 from contextlib import asynccontextmanager
-from db import connect_to_mongo, close_mongo_connection, db
+from db import connect_to_mongo, close_mongo_connection
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await connect_to_mongo()
+    app.db_client = await connect_to_mongo()
     yield
     await close_mongo_connection()
 
 app = FastAPI(lifespan=lifespan)
 
+# Register routes
 app.include_router(base.base_router)
 app.include_router(data.data_router)
