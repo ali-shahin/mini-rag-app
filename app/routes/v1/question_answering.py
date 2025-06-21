bea0fabb61f720d5d3268d076816a01c941acfbe
ali <aly.shaheen14@gmail.com>
2025-06-21 14:44:44 +0300

    Implement answer endpoint

--- app/routes/v1/question_answering.py
+++ app/routes/v1/question_answering.py
@@ -30,4 +30,25 @@ async def search_knowledgebase(request: Request, project_id: str, request_data: 
     if not results:
         return JSONResponse(status.HTTP_400_BAD_REQUEST, content={"message": "No results found."})
     
+    results = [dict(result) for result in results]
+
     return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Data retrieved.", "results": results})
+
+# /api/v1/knowledgebase/answer/{project_id}
+@router.post("/answer/{project_id}")
+async def answer_knowledgebase(request: Request, project_id: str, request_data: SearchRequest):
+    project_repo = await ProjectRepo.create_instance(request.app.db_client)
+    nlp_controller = NlpController(request.app.vector_db_client, request.app.generation_client, request.app.embedding_client)
+
+    project = await project_repo.get_or_create_project(project_id)
+    if project is None:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": f"Project {project_id} not found."}
+        )
+    
+    answer = await nlp_controller.answer_query(project.project_id, request_data.query, request_data.limit)
+    if not answer:
+        return JSONResponse(status.HTTP_400_BAD_REQUEST, content={"message": "No results found."})
+        
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Data retrieved.", "answer": answer})
